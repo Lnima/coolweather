e54c7927f713605004b71a91fcf38ec91c806d19
Lnima <891969737@qq.com>
2018-05-30 20:35:07 +0800

    加入创建数据库的各种配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport{
+
+    private int id;
+
+    private String privinceName;//记录省名字
+
+    private int provinceCode;//记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
